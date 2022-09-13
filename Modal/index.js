f1ccb5ed937a7bf30f1f50c1f26745ac87b1e3d7
fAwkes1101 <abhishekyadav0751@gmail.com>
2022-09-13 21:56:15 +0530

    :tada:

--- Modal/index.js
+++ Modal/index.js
@@ -1,1 +1,9 @@
-console.log("script hi")
+
+document.getElementById("open-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "block";
+})
+
+
+document.getElementById("close-modal").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none";
+})
